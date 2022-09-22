5ff54d789674e9454fc30add268dd4e9557b3699
amed <amedduman@protonmail.com>
2022-09-22 22:48:21 +0300

    Add assignment 5

--- Assets/Assignment_5_MeshCalculator/MeshCalc.cs
+++ Assets/Assignment_5_MeshCalculator/MeshCalc.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class MeshCalc : MonoBehaviour
 {
     [SerializeField] Mesh _mesh;
-
-    void OnDrawGizmos()
+    [SerializeField] float _area;
+    void OnValidate()
     {
-        if(_mesh == null) return;
+        _area = 0;
+        Vector3[] verts = _mesh.vertices;
+        int[] tris = _mesh.triangles;
+
+        for (int i = 0; i < tris.Length; i+= 3)
+        {
+            Vector3 a = verts[tris[i]];
+            Vector3 b = verts[tris[i + 1]];
+            Vector3 c = verts[tris[i + 2]];
+
+            _area += Vector3.Cross(b-a, c-a).magnitude;
+
+        }
+
+        _area /= 2;
     }
 }
